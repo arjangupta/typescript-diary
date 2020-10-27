3c6bb3dbdc6756153790c7824a085071d0f7d6e8
Arjan Gupta <agupta@intellifarms.com>
2020-10-27 11:37:45 -0500

    Use common_declarations.ts with an import * as

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,13 @@
 // Description: Use async-await and setTimeout() to delay displaying messages
 
-// This function returns a promise that is resolved in the
-// given amount of time
+import * as CommonDeclarations from './common_declarations'
+
+// This function returns a promise that is resolved in the given amount of time
 function wait (delay_in_milliseconds: number) {
-    return new Promise((resolve, reject) => setTimeout(resolve, delay_in_milliseconds));
+    return new Promise((resolve, reject) => setTimeout(resolve, delay_in_milliseconds)); // reject is unused
 }
 
-// Callback function type (appended with B in order to not conflict with the type from the other file)
-type StringCallbackFunctionB = (s: string) => void;
-
-// Constant time duration
-const MESSAGE_DELAY_MS: number = 1000;
-
-// Takes a callback function and runs it with different
-// arguments and a fixed delay
-async function startAsync (callback : StringCallbackFunctionB) {
-    await wait(MESSAGE_DELAY_MS);
+// Takes a callback function and runs it with different arguments and a fixed delay
+async function startAsync (callback: CommonDeclarations.StringCallbackFunction) {
+    await wait(CommonDeclarations.MESSAGE_DELAY);
 }
--- src/set_timeout_method.ts
+++ src/set_timeout_method.ts
@@ -1,19 +1,21 @@
 // Description: Use callbacks and setTimeout() to delay displaying messages
 
+import * as CommonDeclarations from './common_declarations'
+
 // "start" is a function that takes another function
 // as its argument (callback). Then, the callback function
 // is run multiple times in the body with different strings.
 // Pre-condition: Callback must be a single string-param function
-function start (callback: StringCallbackFunction) {
+function start (callback: CommonDeclarations.StringCallbackFunction) {
     setTimeout(() => {
         callback('Hello');
         setTimeout(() => {
             callback('My name is');
             setTimeout(() => {
                 callback('Arjan Gupta');
-            }, MESSAGE_DELAY);
-        }, MESSAGE_DELAY);
-    }, MESSAGE_DELAY);
+            }, CommonDeclarations.MESSAGE_DELAY);
+        }, CommonDeclarations.MESSAGE_DELAY);
+    }, CommonDeclarations.MESSAGE_DELAY);
 }
 
 // Pass start a function that takes a string arg
