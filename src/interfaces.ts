fef3f41bee891fe98c240c6a188cdadb8a22c3ce
Arjan Gupta <agupta@intellifarms.com>
2020-11-19 13:34:34 -0600

    Declare a second object in another way and see that it has similar output

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -4,20 +4,27 @@ function interfacesMain() {
     interface Interface1 {
         first: string;
         second?: number;
-    }
+    };
 
     const thing1 = "hello";
     const thing2 = undefined;
 
     const object1 : Interface1 = {
         first: thing1
-    }
+    };
 
     console.log({object1}); // outputs: { object1: { first: 'hello' } }
 
     object1.second = thing2;
 
     console.log({object1}); // outputs: { object1: { first: 'hello', second: undefined } }
+
+    const object2 : Interface1 = {
+        first: thing1,
+        second: thing2
+    };
+
+    console.log({object2}); // outputs: { object2: { first: 'hello', second: undefined } }
 }
 
 interfacesMain();
