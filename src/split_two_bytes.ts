0a503b01b824f9208765b4d2612c5afd0a2d024a
Arjan Gupta <agupta@intellifarms.com>
2021-03-17 11:50:44 -0500

    Define the ff00 and 00ff bytes

--- src/split_two_bytes.ts
+++ src/split_two_bytes.ts
@@ -1,7 +1,13 @@
 import chalk from 'chalk';
 
 function split_two_bytes() {
+    // All 1s in both bytes
     const full_two_bytes : number = 65535; // 0xFFFF
+
+    // All 1s in most significant byte
+    const ff00_two_bytes : number = 65280 // 0xFF00
+    // All 1s in least significant byte
+    const ff_two_bytes : number = 255; // 0x00FF
 }
 
 split_two_bytes();
